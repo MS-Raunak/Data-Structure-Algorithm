1a89738d4f056a73051b625411f088454184d856
MS-Raunak <raunakkr.saha@gmail.com>
2024-07-24 21:36:22 +0530

    best approach to reverse an array

--- array_1d/ReverseArray.java
+++ array_1d/ReverseArray.java
@@ -4,27 +4,37 @@ import java.util.Arrays;
 
 //Print all elements in reverse order
 public class ReverseArray {
-    //Print all elements in reverse order
-    private static void printArrInReverseOrder(int[] arr) {
-        for (int i= arr.length-1; i>=0; i--){
-            System.out.print(arr[i] + ", ");
-        }
-    }
 
     //Reverse an array
     private static void reverseArray(int[] arr) {
-        int newArr[] = new int[arr.length];
-        int index=0;
+        int temp[] = new int[arr.length];
+        int idx=0;
         for (int i= arr.length-1; i>=0; i--){
-            newArr[index++] = arr[i];
+            temp[idx] = arr[i];
+            idx++;
         }
-        System.out.println(Arrays.toString(newArr));
+        System.out.println(Arrays.toString(temp));
+    }
+
+    //Best Approach To Reverse an array: new array not required
+    private static void reverse(int[]arr){
+        int i=0;
+        int j=arr.length-1;
+        while (i<j){
+            int temp=arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 
 
     public static void main(String[] args) {
         int[]arr = {1,2,3,4,5};
-        reverseArray(arr);
-        //printArrInReverseOrder(arr);
+        //reverseArray(arr);
+        reverse(arr);
     }
 }
