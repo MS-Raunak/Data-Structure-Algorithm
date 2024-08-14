bba24bbcacae2238fa906030165237f2a77e7f04
MS-Raunak <raunakkr.saha@gmail.com>
2024-08-15 02:57:15 +0530

    Reverse array upto k steps**

--- array_1d/ReverseFirstNEleOfArr.java
+++ array_1d/ReverseFirstNEleOfArr.java
@@ -1,2 +1,34 @@
-package array_1d;public class ReverseFirstNEleOfArr {
+package array_1d;
+
+import java.util.Arrays;
+
+public class ReverseFirstNEleOfArr {
+    public static void main(String[] args) {
+        int[]arr = {2,3,4,5,6};
+        int n = 2;
+
+        reverse(arr, n);
+    }
+
+    private static void reverse(int[]arr, int n){
+        int count=0;
+        int start=0;
+        int end = arr.length-1;
+
+        while (start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+            count++;
+
+            if (count==n) {
+                System.out.println(Arrays.toString(arr));
+                return;
+            }
+
+        }
+    }
 }
