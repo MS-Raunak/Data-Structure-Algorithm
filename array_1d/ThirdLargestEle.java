f181b1600f28f7d77049568ffacfc97fafa9b415
MS-Raunak <raunakkr.saha@gmail.com>
2024-08-15 02:59:04 +0530

    3rd largest element of an array**

--- array_1d/ThirdLargestEle.java
+++ array_1d/ThirdLargestEle.java
@@ -1,2 +1,31 @@
-package array_1d;public class ThirdLargestEle {
+package array_1d;
+
+public class ThirdLargestEle {
+    public static void main(String[] args) {
+        int[]arr = {5,2,7,9,10,12};
+        thirdMaxEle(arr);
+    }
+
+    private static void thirdMaxEle(int[] arr) {
+        int max=0;
+        int secMax=0;
+        int thrdMax=0;
+
+        for(int ele : arr){
+            if (ele > max){
+                thrdMax = secMax;
+                secMax = max;
+                max = ele;
+            }
+            else if (ele > secMax && ele < max){
+                thrdMax = secMax;
+                secMax = ele;
+            }
+            else{  //(ele > thrdMax && ele < secMax)
+                thrdMax = ele;
+            }
+        }
+
+        System.out.println(thrdMax);
+    }
 }
