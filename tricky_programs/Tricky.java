5b4628013afde3cb061e4446e8730d9474519d20
MS-Raunak <raunakkr.saha@gmail.com>
2024-08-15 03:07:55 +0530

    Print 1 to 100 without using loop

--- tricky_programs/Tricky.java
+++ tricky_programs/Tricky.java
@@ -5,7 +5,13 @@ public class Tricky {
         //print hello world without using semicolon
         if (System.out.printf("Hello World")==null){}
 
+        count(1);
     }
 
-
+    //print 1 to 100 without using loop
+    private static void count(int num){
+        if (num>100) return;
+        System.out.println(num);
+        count(num+1);
+    }
 }
